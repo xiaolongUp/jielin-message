d8856fc6699ac96de7a44fc26fb53351ab5377b7
xlyang <1647097651@qq.com>
2019-10-24 11:29:54 +0800

    消费数据类型改为String

--- src/main/java/com/jielin/message/service/AsynPushMsgService.java
+++ src/main/java/com/jielin/message/service/AsynPushMsgService.java
@@ -1,5 +1,6 @@
 package com.jielin.message.service;
 
+import com.google.gson.Gson;
 import com.jielin.message.dto.ParamDto;
 import com.jielin.message.util.MsgConstant;
 import lombok.extern.slf4j.Slf4j;
@@ -20,11 +21,22 @@ public class AsynPushMsgService {
 
     @Autowired
     private SynMsgPushService synMsgPushService;
+    @Autowired
+    private Gson gson;
 
+    //本系统需要投递的数据，直接通过对象投递
     @RabbitHandler
     public void process(ParamDto paramDto) {
-        synMsgPushService.push(paramDto);
+        //synMsgPushService.push(paramDto);
         log.info("Receiver  : {}", paramDto.toString());
+    }
+
+    //所有其他系统投递的数据都为字符转
+    @RabbitHandler
+    public void process(String paramDto) {
+        ParamDto param = gson.fromJson(paramDto, ParamDto.class);
+        synMsgPushService.push(param);
+        log.info("Receiver  : {}", paramDto);
     }
 
 }
